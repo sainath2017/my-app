8a83432316935e17c64b49acf701dd2fd072048f
Mutakoduru Sainath <Mutakodurusainath@gmail.com>
2019-03-21 00:46:50 -0400

    initial commit

---
+++ src/app/utils/app-init.ts
@@ -0,0 +1,17 @@
+import { KeycloakService } from 'keycloak-angular';
+
+export function initializer(keycloak: KeycloakService): () => Promise<any> {
+  return (): Promise<any> => keycloak.init({
+    config: {
+      url: 'http://localhost:8180/auth',
+      realm: 'DEV_INT',
+      clientId: 'my-app'
+    },
+    initOptions: {
+      onLoad: 'login-required',
+      checkLoginIframe: false
+    },
+    enableBearerInterceptor: true,
+    bearerExcludedUrls: ['/assets', '/clients/public']
+  });
+}
